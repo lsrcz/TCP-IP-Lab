23822ad3b4184c1cbc50ab299bbf5f40759f4fa8
lsrcz <lsrcz@pku.edu.cn>
2018-11-03 17:40:31 +0000

    test wrapper

--- src/test/testWrapper.cpp
+++ src/test/testWrapper.cpp
@@ -1,24 +1,32 @@
 #include <wrapper/unix_file.h>
+#include <wrapper/pcap.h>
 #include <sys/stat.h>
 #include <stdio.h>
-
-int a() {
-    Close(10, ErrorBehavior("This should return -1", true, RETURN_MINUS_ONE));
-    return 0;
-}
+#include <protocol/device.h>
 
 int b() {
-    Close(10, ErrorBehavior("Something will be printed", false, USER_ACTION), printf("1234., return -3\n");return -3);
+    Close(10, ErrorBehavior("Something will be printed", false, true), printf("1234., return -3\n");return -3);
+    return 1;
 }
 
 int main() {
+    char buf[1000];
+    for (auto &p: listAvailableDevices()) {
+        printf("%s\n", p.c_str());
+    }
+
+
+    pcap_t * p = Pcap_create("ens33", buf);
+    if (p == NULL)
+        printf("is null");
+    Pcap_activate(p);
+
     Close(10,10);
     Open("/tmp/aaaa", O_WRONLY);
     Open("/root/aaa", O_CREAT, 0);
-    Open("/tmp/aaaa", O_WRONLY, ErrorBehavior("User msg can be printed", false, USER_ACTION), printf("User action can be executed\n"));
+    Open("/tmp/aaaa", O_WRONLY, ErrorBehavior("User msg can be printed", false, true), printf("User action can be executed\n"));
     Close(10);
-    printf("should be -1: %d\n", a());
     printf("should be -3: %d\n", b());
-    Close(10, ErrorBehavior("This should be fatal and no more would be printed, and the program will end with -1", true, EXIT_MINUS_ONE));
+    Close(10, ErrorBehavior("This should be fatal and no more would be printed, and the program will end with -1", true, true), return -1);
     Close(10);
 }
